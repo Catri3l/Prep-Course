0013af7a937422177b586d3162d08c0f707da454
Catri3l <117287838+Catri3l@users.noreply.github.com>
2022-11-19 16:51:33 -0300

    primer test v3

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -212,7 +212,7 @@ return nombre + " " + apellido
 }
 
 function obtenerSaludo(nombre) {
-  // Toma la string "nombre" y concatena otras string en gla cadena para que tome la siguiente forma:
+  // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
   return 'Hola ' + nombre + '!';
